97e58823555dead7b9e79f42f3e7832489e55f9c
Aleksandr Bezobchuk <alex@tendermint.com>
2019-01-11 12:41:29 -0500

    Implement memstore and corresponding unit tests

---
+++ storage/mem_store.go
@@ -0,0 +1,167 @@
+package storage
+
+import (
+	"encoding/binary"
+	"fmt"
+	"sync"
+
+	"github.com/alexanderbez/goraft/types"
+)
+
+// Assert that RaftStore implements the RaftDB interface at compile time.
+var _ RaftDB = (*MemStore)(nil)
+
+type (
+	// MemStore implements an in-memory simple Raft stable storage and persistance
+	// layer. It implements the RaftDB interface.
+	MemStore struct {
+		mtx sync.RWMutex
+
+		lastLogIndex uint64 // log with the highest index
+
+		logsBucket map[uint64]types.Log // persistance of append-only logs
+		mainBucket map[string][]byte    // persistance of non-log data (e.g. cluster config and node metadata)
+	}
+)
+
+// ----------------------------------------------------------------------------
+// MemStore
+
+func NewMemStore() *MemStore {
+	return &MemStore{
+		logsBucket: make(map[uint64]types.Log),
+		mainBucket: make(map[string][]byte),
+	}
+}
+
+// SetLogEncoder sets the store's log encoder type.
+func (ms *MemStore) SetLogEncoder(_ types.LogEncoder) {}
+
+// SetLogDecoder sets the store's log decoder type.
+func (ms *MemStore) SetLogDecoder(_ types.LogDecoder) {}
+
+// Get returns a value for a given key. If the key does not exist, an error is
+// returned.
+func (ms *MemStore) Get(key []byte) ([]byte, error) {
+	ms.mtx.RLock()
+	defer ms.mtx.RUnlock()
+
+	value, ok := ms.mainBucket[string(key)]
+	if !ok {
+		return nil, ErrKeyNotFound
+	}
+
+	return value, nil
+}
+
+// Set inserts a value for a given key. An error is returned upon failure. An
+// error is returned upon failure.
+func (ms *MemStore) Set(key, value []byte) error {
+	ms.mtx.Lock()
+	defer ms.mtx.Unlock()
+
+	ms.mainBucket[string(key)] = value
+	return nil
+}
+
+// GetUint64 returns an unsigned 64-bit value for a given key. If the key does
+// not exist, an error is returned.
+func (ms *MemStore) GetUint64(key []byte) (uint64, error) {
+	ms.mtx.RLock()
+	defer ms.mtx.RUnlock()
+
+	value, ok := ms.mainBucket[string(key)]
+	if !ok {
+		return 0, ErrKeyNotFound
+	}
+
+	return binary.LittleEndian.Uint64(value), nil
+}
+
+// SetUint64 inserts an unsigned 64-bit value for a given key. An error is
+// returned upon failure.
+func (ms *MemStore) SetUint64(key []byte, value uint64) error {
+	ms.mtx.Lock()
+	defer ms.mtx.Unlock()
+
+	ms.mainBucket[string(key)] = uint64ToBytes(value)
+	return nil
+}
+
+// SetLogs inserts a batch of logs where each log should contain an incremented
+// index. An error is returned if any log cannot be serialized or if insertion
+// fails.
+//
+// Contract: No log should exist for that given index.
+func (ms *MemStore) SetLogs(logs []types.Log) error {
+	ms.mtx.Lock()
+	defer ms.mtx.Unlock()
+
+	for _, log := range logs {
+		if log.Index > ms.lastLogIndex {
+			ms.lastLogIndex = log.Index
+		}
+
+		ms.logsBucket[log.Index] = log
+	}
+
+	return nil
+}
+
+// SetLog inserts a log by index. An error is returned if the log cannot be
+// serialized or if insertion fails.
+//
+// Contract: No log should exist for that given index.
+func (ms *MemStore) SetLog(log types.Log) error {
+	return ms.SetLogs([]types.Log{log})
+}
+
+// GetLog returns a log for a given index. If no log exists for such an index
+// or if it cannot be deserialized, an error is returned.
+func (ms *MemStore) GetLog(index uint64) (types.Log, error) {
+	ms.mtx.RLock()
+	defer ms.mtx.RUnlock()
+
+	log, ok := ms.logsBucket[index]
+	if !ok {
+		return log, ErrKeyNotFound
+	}
+
+	return log, nil
+}
+
+// GetLastLogIndex returns the last stored log index. If no logs have been
+// stored, then zero is returned.
+func (ms *MemStore) GetLastLogIndex() uint64 {
+	ms.mtx.RLock()
+	defer ms.mtx.RUnlock()
+
+	return ms.lastLogIndex
+}
+
+// LogsIter iterates over a given range of logs by an index range. For each
+// index, a cb function that accepts the given log is called. If the callback
+// executed for any given log returns an error, the iteration exits along with
+// the error being returned to the caller.
+func (ms *MemStore) LogsIter(start, end uint64, cb func(log types.Log) error) error {
+	ms.mtx.RLock()
+	defer ms.mtx.RUnlock()
+
+	if start > end {
+		return fmt.Errorf("invalid range query: %d > %d", start, end)
+	}
+
+	for i := start; i <= end; i++ {
+		log, ok := ms.logsBucket[i]
+		if ok {
+			if err := cb(log); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+// Close performs a no-op on a MemStore.
+func (ms *MemStore) Close() error { return nil }
---
+++ storage/mem_store_test.go
@@ -0,0 +1,152 @@
+package storage_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/alexanderbez/goraft/storage"
+	"github.com/alexanderbez/goraft/types"
+)
+
+func TestMemStore(t *testing.T) {
+	memStore := storage.NewMemStore()
+	require.NotNil(t, memStore)
+
+	err := memStore.Close()
+	require.NoError(t, err)
+}
+
+func TestMemStoreGetSet(t *testing.T) {
+	memStore := storage.NewMemStore()
+	require.NotNil(t, memStore)
+
+	key, value := []byte("nodeID"), []byte("node0")
+
+	// require non-existant key returns nil
+	res, err := memStore.Get(key)
+	require.Error(t, err)
+	require.Nil(t, res)
+
+	err = memStore.Set(key, value)
+	require.NoError(t, err)
+
+	// require existant key returns correct value
+	res, err = memStore.Get(key)
+	require.NoError(t, err)
+	require.Equal(t, value, res)
+}
+
+func TestMemStoreGetSetUint64(t *testing.T) {
+	memStore := storage.NewMemStore()
+	require.NotNil(t, memStore)
+
+	key, value := []byte("commitID"), uint64(71)
+
+	// require non-existant key returns nil
+	res, err := memStore.GetUint64(key)
+	require.Error(t, err)
+	require.Equal(t, uint64(0), res)
+
+	err = memStore.SetUint64(key, value)
+	require.NoError(t, err)
+
+	// require existant key returns correct value
+	res, err = memStore.GetUint64(key)
+	require.NoError(t, err)
+	require.Equal(t, value, res)
+}
+
+func TestMemStoreSetLogs(t *testing.T) {
+	memStore := storage.NewMemStore()
+	require.NotNil(t, memStore)
+
+	logs := make([]types.Log, 20)
+	for i := range logs {
+		logs[i] = types.NewLog(uint64(i), 1, nil)
+	}
+
+	err := memStore.SetLogs(logs)
+	require.NoError(t, err)
+
+	// require all logs exist that were inserted in batch
+	for i, log := range logs {
+		res, err := memStore.GetLog(uint64(i))
+		require.NoError(t, err)
+		require.Equal(t, log, res)
+	}
+}
+
+func TestMemStoreGetSetLog(t *testing.T) {
+	memStore := storage.NewMemStore()
+	require.NotNil(t, memStore)
+
+	log := types.NewLog(1, 1, []byte("command"))
+
+	// require non-existant key returns nil
+	res, err := memStore.GetLog(log.Index)
+	require.Error(t, err)
+	require.Equal(t, storage.ErrKeyNotFound, err)
+	require.Equal(t, types.Log{}, res)
+
+	err = memStore.SetLog(log)
+	require.NoError(t, err)
+
+	// require existant key returns correct log
+	res, err = memStore.GetLog(log.Index)
+	require.NoError(t, err)
+	require.Equal(t, log, res)
+}
+
+func TestMemStoreGetLastLogIndex(t *testing.T) {
+	memStore := storage.NewMemStore()
+	require.NotNil(t, memStore)
+
+	llIndex := memStore.GetLastLogIndex()
+	require.Equal(t, uint64(0), llIndex)
+
+	logs := make([]types.Log, 20)
+	for i := range logs {
+		logs[i] = types.NewLog(uint64(i), 1, nil)
+	}
+
+	for _, log := range logs {
+		err := memStore.SetLog(log)
+		require.NoError(t, err)
+	}
+
+	llIndex = memStore.GetLastLogIndex()
+	require.Equal(t, logs[len(logs)-1].Index, llIndex)
+}
+
+func TestMemStoreLogsIter(t *testing.T) {
+	memStore := storage.NewMemStore()
+	require.NotNil(t, memStore)
+
+	logs := make([]types.Log, 20)
+	for i := range logs {
+		logs[i] = types.NewLog(uint64(i), 1, nil)
+	}
+
+	for _, log := range logs {
+		err := memStore.SetLog(log)
+		require.NoError(t, err)
+	}
+
+	// create an iterator and retrieve all log indices
+	var indices []uint64
+	err := memStore.LogsIter(
+		logs[0].Index,
+		logs[len(logs)-1].Index,
+		func(log types.Log) error {
+			indices = append(indices, log.Index)
+			return nil
+		},
+	)
+	require.NoError(t, err)
+
+	// require all log indices are correct
+	for i, index := range indices {
+		require.Equal(t, index, logs[i].Index)
+	}
+}
